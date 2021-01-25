22488be2ca9dbb4052bce5a1761ce0a37ee4d6e3
root <root@ip-172-31-27-2.ec2.internal>
2021-01-25 06:56:22 +0000

    Updated

--- dock-compose.sh
+++ dock-compose.sh
@@ -6,3 +6,9 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 apt install docker-compose -y
+sudo apt-get install unzip -y
+sudo apt-get install libwww-perl libdatetime-perl -y
+curl https://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.2.zip -O
+unzip CloudWatchMonitoringScripts-1.2.2.zip
+rm CloudWatchMonitoringScripts-1.2.2.zip && 
+cd aws-scripts-mon
